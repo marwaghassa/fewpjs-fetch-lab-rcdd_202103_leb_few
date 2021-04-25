064b2907c87eb2a87ac2e7b883de0e4a3dfe13c8
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-25 20:35:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   return fetch('https://anapioficeandfire.com/api/books')
-  .then()
+  .then(resp )
   .then()
 }
 
